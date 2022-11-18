71e07364138c7ba52bfdcdaf985eb17510bbdc1f
Regislaine <nanafidelis@gmail.com>
2022-11-18 16:23:32 -0300

    feat: criacao da pasta e criacao do arquivo para elaboração do desafio

---
+++ primeiras-paginas-interativas-com-javascript/aula-1/desafio1.js
@@ -0,0 +1,10 @@
+/* Faça um programa para calcular o valor gasto de uma viagem.
+
+    Você terá três variáveis, sendo elas:
+    1 - Preço combustível;
+    2 - Gasto médio de combustível do carro por KM;
+    3 - Distância em KM da viagem;
+
+Imprima no console o valor que será gasto para realizar esta viagem
+*/
+
