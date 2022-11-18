d9e0266109f00e94f4a7f068b7d005bebef8f0ce
Regislaine <nanafidelis@gmail.com>
2022-11-18 17:23:08 -0300

    feat: Realização do desafio 2 da aula 2

--- primeiras-paginas-interativas-com-javascript/aula-2/desafio.js
+++ primeiras-paginas-interativas-com-javascript/aula-2/desafio.js
@@ -10,3 +10,17 @@ Faça um programa para calcular o valor de uma viagem.
 
 Imprima no console o valor que será gasto para realizar esta viagem.
 */
+
+const precoEtanol = 5.02;
+const precoGasolina = 6.03;
+const kmPorLitro = 14;
+const distanciaEmKm = 100;
+const tipoCombustivel = 'Etanol';
+
+if (tipoCombustivel === 'Etanol') {
+    const valorGasto = (distanciaEmKm/ kmPorLitro) * precoEtanol;
+    console.log("O valor gasto para realizar a viagem é de:", valorGasto.toFixed(2));
+} else {
+    const valorGasto = (distanciaEmKm/ kmPorLitro) * precoGasolina;
+    console.log("O valor gasto para realizar a viagem é de:", valorGasto.toFixed(2));
+}
