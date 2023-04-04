a46077d8ec080085fc0c2b0b71a6ce533e5ec861
Regislaine <nanafidelis@gmail.com>
2023-04-03 21:17:35 -0300

    realização do exercicio de loop criando uma tabuada

---
+++ arrays-e-estruturas-de-repeticao/exercicio1.js
@@ -0,0 +1,9 @@
+/*
+  1 - crie um programa que dado um número imprima a sua tabuada.
+*/
+
+const numero = 2;
+
+for (let index = 1; index <= 10; index += 1) {
+  console.log(`${index} x ${numero} = `, index*numero);
+}
